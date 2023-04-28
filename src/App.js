04662f1f3aae4cf8a1961220ba5067a23bca8cfb
RustemAbdulaev <rustem.abdulaev@gmail.com>
2023-04-29 00:39:31 +0300

    Slots,inputFields,Button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    accordionDetailsClasses,
+    Box,
+    Button,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Paper,
+    Select,
+    TextField
+} from "@mui/material";
+import axios from "axios";
+import {useEffect, useState} from "react";
+import {grey} from "@mui/material/colors";
+import Slots from "./Components/Slots/Slots";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user_id, setUser_id] = useState('')
+    const [doctor_id, setDoctor_id] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
+    const [currentDoctor, setCurrentDoctor] = useState({})
+    console.log(currentDoctor?.slots)
+
+    const getDoctorInfo = async () => {
+        const {data} = await axios.get(`http://localhost:5000/slot/getDoctor/${doctor_id}`)
+        console.log(data)
+        setCurrentDoctor(data)
+
+    }
+    useEffect(() => {
+        getDoctorInfo()
+    }, [doctor_id])
+
+    const dataToSend = {
+        user_id,
+        doctor_id,
+        slots: date
+    }
+    const reserveSlot = async () => {
+        console.log(dataToSend)
+        const {data} = await axios.post('http://localhost:5000/slot/slotAdd', dataToSend)
+        console.log(data)
+        console.log(dataToSend)
+        setUser_id('')
+        setDoctor_id('')
+        setDate('')
+        setTime('')
+    }
+    return (
+        <div className="App">
+            <Box display={"flex"} justifyContent={"center"} alignItems={"center"} flexDirection={"column"}>
+            <Slots currentDoctor={currentDoctor}/>
+                <Box display={'flex'} justifyContent={'center'} m={5} gap={3}>
+                    <TextField value={user_id} onChange={(e) => {setUser_id(e.target.value)
+                    }} id="outlined-basic" label="User ID" variant="outlined"/>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Doctor</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={doctor_id}
+                            label="Doctor"
+                            onChange={e => setDoctor_id(e.target.value)}
+                        >
+                            <MenuItem value={'644bf49b38585848c85b8c09'}>Therapeht</MenuItem>
+                            <MenuItem value={'644c14c006d58328f3aeedf0'}>Uho</MenuItem>
+                            <MenuItem value={'644c2cb3f1a313edcf712009'}>Nos</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <TextField type={"date"} value={date} onChange={(e) => setDate(e.target.value)} id="outlined-basic"
+                               variant="outlined"/>
+                    <TextField type={"time"} value={time} onChange={(e) => setTime(e.target.value)} id="outlined-basic"
+                               variant="outlined"/>
+                </Box>
+            </Box>
+            <Button variant={'contained'} onClick={reserveSlot}>
+                Записаться
+            </Button>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Components/Slots/Slots.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Box, Paper} from "@mui/material";
+import {grey} from "@mui/material/colors";
+
+const Slots = (props) => {
+    return (
+        <Box sx={{
+            display:'flex',
+            justifyContent:'center',
+            alignItems:'center',
+            width:500,
+            backgroundColor:'yellow'}
+        }>
+            {props.currentDoctor?.slots?.map(v=>
+                <Paper sx={{
+                    margin:2,
+                    width: '150px',
+                    height: '50px',
+                    backgroundColor: grey["200"],
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }} elevation={2}>{v.date_time.start}</Paper>
+            )}
+        </Box>
+    );
+};
+
+export default Slots;
