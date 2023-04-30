42cb6eca765ddb8578524f2734fced515299058f
RustemAbdulaev <rustem.abdulaev@gmail.com>
2023-04-30 13:24:27 +0300

    Working project on remote server

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL:'http://localhost:5000/',
-    // baseURL:'https://listofpurchasesserver.onrender.com/',
+    // baseURL:'http://localhost:5000/',
+    baseURL:'https://doctor-appointment-server.onrender.com/',
 })
 export default instance;
